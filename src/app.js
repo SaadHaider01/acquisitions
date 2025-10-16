f6eaa6a849add0e86f0bd15496837799a86317d6
SaadHaider01 <saadhaider349@gmail.com>
2025-10-16 14:14:22 +0530

    Setup Neon Postgres with drizzle

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from aquisitions');
+  res.send('Hello from acquisitions');
 });
 
 export default app;
